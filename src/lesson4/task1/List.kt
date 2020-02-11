8b22075db2a6d6b09daf973ffa751b362f31cfe5
leerover410 <54933673+leerover410@users.noreply.github.com>
2020-02-11 17:38:21 +0300

    Add files via upload

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -3,6 +3,7 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 /**
@@ -152,7 +153,13 @@ fun center(list: MutableList<Double>): MutableList<Double> {
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int {
+    var c = 0
+    for (i in a.indices) {
+        c += a[i] * b[i]
+    }
+    return c
+}
 
 /**
  * Средняя
@@ -162,7 +169,15 @@ fun times(a: List<Int>, b: List<Int>): Int = TODO()
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0 при любом x.
  */
-fun polynom(p: List<Int>, x: Int): Int = TODO()
+fun polynom(p: List<Int>, x: Int): Int {
+    val def = x.toDouble()
+    var sum = 0
+    for (i in 0 until p.size) {
+        sum += p[i] * def.pow(i).toInt()
+    }
+    return sum
+}
+
 
 /**
  * Средняя
@@ -174,7 +189,14 @@ fun polynom(p: List<Int>, x: Int): Int = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun accumulate(list: MutableList<Int>): MutableList<Int> = TODO()
+fun accumulate(list: MutableList<Int>): MutableList<Int> {
+    var a: Int
+    for (i in 1 until list.size) {
+        a = list[i - 1]
+        list[i] += a
+    }
+    return list
+}
 
 /**
  * Средняя
@@ -223,7 +245,20 @@ fun convertToString(n: Int, base: Int): String = TODO()
  * из системы счисления с основанием base в десятичную.
  * Например: digits = (1, 3, 12), base = 14 -> 250
  */
-fun decimal(digits: List<Int>, base: Int): Int = TODO()
+fun decimal(digits: List<Int>, base: Int): Int {
+    var a = 0
+    var b = 1
+    var c = digits.size
+    while (c > 1) {
+        b *= base
+        c--
+    }
+    for (i in 0 until digits.size) {
+        a += digits[i] * b
+        b /= base
+    }
+    return a
+}
 
 /**
  * Сложная
